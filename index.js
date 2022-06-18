83d0653383310abda30a117c3cf46acc577b4814
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-18 02:21:32 -0400

    commit tree e9dd24bf5609a15d04b664cb7e8eae7f730bff04

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord = word.split('').reverse().join('');
+  return reverseWord === word;
 }
 
 /* 
   Add your pseudocode here
-*/
+*/ // call on function isPalindrome
+  //find if the argument is a palindrome or not
+  //return false/true depending on end value (palindrome or not)
 
 /*
   Add written explanation of your solution here
-*/
+*/ //The argument is taken in and used to make a new variable that is turned into an array of each character in the word, reversed in order, and joins them back together into a new string at the end.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
